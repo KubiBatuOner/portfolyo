ff460971b04d560a3d4c285a415f52b499435ca5
KubiBatuOner <kbatuhanoner@yahoo.com>
2023-03-15 12:15:18 +0300

    Reqres eklendi

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,7 +1,23 @@
+import React from "react";
 import Project from "./Project";
 import { projects } from "./data";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 export default function Projects() {
+  const [data, setData] = useState([]);
+  const [error, setError] = useState();
+
+  useEffect(() => {
+    axios
+      .post("https://reqres.in/api/users", projects)
+      .then((res) => setData(res.data))
+      .catch((error) => {
+        console.log(error);
+        setError(error);
+      });
+  }, []);
+
   return (
     <div
       className="w-[80%] my-0 mx-auto flex flex-col gap-y-[5vh] pt-[42px] pb-[100px] sm:w-[100%] lg:pb-[30px]"
@@ -11,7 +27,7 @@ export default function Projects() {
         Projects
       </h2>
       <div className="flex flex-wrap justify-between items-start lg:flex-col lg:items-center lg:gap-y-[10vh]">
-        {projects.map((project) => (
+        {data.map((project) => (
           <Project key={project.id} project={project} />
         ))}
       </div>
